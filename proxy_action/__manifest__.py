b28cb6a60c41d8c624f9d174eda48ef8cd0bb83b
Yangtze <along008@gmail.com>
2022-09-20 09:00:35 -0700

    fix

--- proxy_action/__manifest__.py
+++ proxy_action/__manifest__.py
@@ -4,12 +4,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Proxy Action",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.1",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/akretion/ak-odoo-incubator",
     "license": "AGPL-3",
     "category": "Generic Modules",
     "depends": ["web"],
-    "data": ["security/ir.model.access.csv", "views/assets.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+             ],
     "installable": True,
+    "assets": {
+        "web.assets_backend": [
+            "/proxy_action/static/src/js/proxy_view.js",
+        ],
+    },
 }
