6c7ee569e68cee22be43a3d11b588d4b7dcf298b
odoochain <75811889+odoochain@users.noreply.github.com>
2023-09-08 21:48:31 +0800

    update

--- label_wizard/__manifest__.py
+++ label_wizard/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://github.com/akretion/ak-odoo-incubator",
     "category": "Custom",
     "summary": "Wizard for choosing how many labels to print",
-    "depends": ["proxy_action"],
+    "depends": ["proxy_action", "stock"],
     "data": [
         "wizard/wizard_view.xml",
         "security/ir.model.access.csv",
